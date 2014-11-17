1042a416890e3bb7c2d2a440adbd3e090011dd21
Aleksandr Kolobanov <shadgm05@gmail.com>
2014-11-17 16:02:38 +0300

    set time params different from defaults

--- library/src/main/java/fr/castorflex/android/smoothprogressbar/ContentLoadingSmoothProgressBar.java
+++ library/src/main/java/fr/castorflex/android/smoothprogressbar/ContentLoadingSmoothProgressBar.java
@@ -10,101 +10,110 @@ import android.view.View;
  */
 public class ContentLoadingSmoothProgressBar extends SmoothProgressBar {
 
-  private static final int MIN_SHOW_TIME = 500; // ms
-  private static final int MIN_DELAY = 500; // ms
+	private static final int DEFAULT_SHOW_TIME = 500; // ms
+	private static final int DEFAULT_DELAY = 500; // ms
 
-  private long mStartTime = -1;
+	private long mShowTime = DEFAULT_SHOW_TIME;
+	private long mStartTime = -1;
 
-  private boolean mPostedHide = false;
+	private boolean mPostedHide = false;
 
-  private boolean mPostedShow = false;
+	private boolean mPostedShow = false;
 
-  private boolean mDismissed = false;
+	private boolean mDismissed = false;
 
-  private final Runnable mDelayedHide = new Runnable() {
+	private final Runnable mDelayedHide = new Runnable() {
 
-    @Override
-    public void run() {
-      mPostedHide = false;
-      mStartTime = -1;
-      setVisibility(View.GONE);
-    }
-  };
+		@Override
+		public void run() {
+			mPostedHide = false;
+			mStartTime = -1;
+			setVisibility(View.GONE);
+		}
+	};
 
-  private final Runnable mDelayedShow = new Runnable() {
+	private final Runnable mDelayedShow = new Runnable() {
 
-    @Override
-    public void run() {
-      mPostedShow = false;
-      if (!mDismissed) {
-        mStartTime = System.currentTimeMillis();
-        setVisibility(View.VISIBLE);
-      }
-    }
-  };
+		@Override
+		public void run() {
+			mPostedShow = false;
+			if (!mDismissed) {
+				mStartTime = System.currentTimeMillis();
+				setVisibility(View.VISIBLE);
+			}
+		}
+	};
 
-  public ContentLoadingSmoothProgressBar(Context context) {
-    this(context, null);
-  }
+	public ContentLoadingSmoothProgressBar(Context context) {
+		this(context, null);
+	}
 
-  public ContentLoadingSmoothProgressBar(Context context, AttributeSet attrs) {
-    super(context, attrs, 0);
-  }
+	public ContentLoadingSmoothProgressBar(Context context, AttributeSet attrs) {
+		super(context, attrs, 0);
+	}
 
-  @Override
-  public void onAttachedToWindow() {
-    super.onAttachedToWindow();
-    removeCallbacks();
-  }
+	public void setShowTime(long ShowTime) {
+		this.mShowTime = ShowTime;
+	}
 
-  @Override
-  public void onDetachedFromWindow() {
-    super.onDetachedFromWindow();
-    removeCallbacks();
-  }
+	@Override
+	public void onAttachedToWindow() {
+		super.onAttachedToWindow();
+		removeCallbacks();
+	}
 
-  private void removeCallbacks() {
-    removeCallbacks(mDelayedHide);
-    removeCallbacks(mDelayedShow);
-  }
+	@Override
+	public void onDetachedFromWindow() {
+		super.onDetachedFromWindow();
+		removeCallbacks();
+	}
 
-  /**
-   * Hide the progress view if it is visible. The progress view will not be
-   * hidden until it has been shown for at least a minimum show time. If the
-   * progress view was not yet visible, cancels showing the progress view.
-   */
-  public void hide() {
-    mDismissed = true;
-    removeCallbacks(mDelayedShow);
-    long diff = System.currentTimeMillis() - mStartTime;
-    if (diff >= MIN_SHOW_TIME || mStartTime == -1) {
-      // The progress spinner has been shown long enough
-      // OR was not shown yet. If it wasn't shown yet,
-      // it will just never be shown.
-      setVisibility(View.GONE);
-    } else {
-      // The progress spinner is shown, but not long enough,
-      // so put a delayed message in to hide it when its been
-      // shown long enough.
-      if (!mPostedHide) {
-        postDelayed(mDelayedHide, MIN_SHOW_TIME - diff);
-        mPostedHide = true;
-      }
-    }
-  }
+	private void removeCallbacks() {
+		removeCallbacks(mDelayedHide);
+		removeCallbacks(mDelayedShow);
+	}
 
-  /**
-   * Show the progress view after waiting for a minimum delay. If
-   * during that time, hide() is called, the view is never made visible.
-   */
-  public void show() {
-    // Reset the start time.
-    mStartTime = -1;
-    mDismissed = false;
-    removeCallbacks(mDelayedHide);
-    if (!mPostedShow) {
-      postDelayed(mDelayedShow, MIN_DELAY);
-      mPostedShow = true;
-    }
-  }
+	/**
+	 * Hide the progress view if it is visible. The progress view will not be
+	 * hidden until it has been shown for at least a minimum show time. If the
+	 * progress view was not yet visible, cancels showing the progress view.
+	 */
+	public void hide() {
+		mDismissed = true;
+		removeCallbacks(mDelayedShow);
+		long diff = System.currentTimeMillis() - mStartTime;
+		if (diff >= mShowTime || mStartTime == -1) {
+			// The progress spinner has been shown long enough
+			// OR was not shown yet. If it wasn't shown yet,
+			// it will just never be shown.
+			setVisibility(View.GONE);
+		} else {
+			// The progress spinner is shown, but not long enough,
+			// so put a delayed message in to hide it when its been
+			// shown long enough.
+			if (!mPostedHide) {
+				postDelayed(mDelayedHide, mShowTime - diff);
+				mPostedHide = true;
+			}
+		}
+	}
+
+	/**
+	 * Show the progress view after waiting for a minimum delay. If
+	 * during that time, hide() is called, the view is never made visible.
+	 */
+	public void show(int startDelay) {
+		// Reset the start time.
+		mStartTime = -1;
+		mDismissed = false;
+		removeCallbacks(mDelayedHide);
+		if (!mPostedShow) {
+			postDelayed(mDelayedShow, startDelay);
+			mPostedShow = true;
+		}
+	}
+
+	public void show() {
+		show(DEFAULT_DELAY);
+	}
 }
